523ab781f76de763dcb2a4d38443927bd9cd074e
vethsivhour <vethsivhour@gmail.com>
2025-12-27 09:28:01 +0700

    finished history and research-interest components

--- src/view/AboutLab.vue
+++ src/view/AboutLab.vue
@@ -1,13 +1,20 @@
 <template>
+    <div class="cover-container">
+        <img src="@/assets/about-us/cover.png" alt="Cover Image" class="cover-image" />
+    </div>
+    <History />
     <HOL_Card />    
     <OurVision />
     <OurService :services="servicesData" />
+    <ResearchInterests />
 </template>
 
 <script setup>
 import OurVision from '@/components/about-us/our-vision.vue'
 import HOL_Card from '../components/about-us/HOL_Card.vue';
 import OurService from '@/components/about-us/our-service.vue';
+import History from '@/components/about-us/history.vue';
+import ResearchInterests from '@/components/about-us/research-interests.vue';
 
 // Import assets for services
 import service1 from '@/assets/our-service/service1.png'
@@ -32,6 +39,21 @@ const servicesData = [
 const components = {
     OurVision,
     HOL_Card,
-    OurService
+    OurService,
+    History,
+    ResearchInterests
 }
 </script>
+
+<style scoped>
+.cover-container {
+    width: 100%;
+    margin-bottom: 60px;
+}
+
+.cover-image {
+    width: 100%;
+    height: auto;
+    object-fit: cover;
+}
+</style>
